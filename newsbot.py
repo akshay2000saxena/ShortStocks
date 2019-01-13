4116781701d61c03b331283f98c78a45bbb8cc8f
Akshay Saxena <41586370+akshay2000saxena@users.noreply.github.com>
2019-01-13 18:52:36 +0530

    Add files via upload

--- newsbot.py
+++ newsbot.py
@@ -14,15 +14,6 @@ def getarticle(url):
     return final
 
 def getnews(news):
-        #Get company stock price
-        quote_page = 'https://m.nasdaq.com/symbol/' + news
-        page = urlopen(quote_page)
-        soup = BeautifulSoup(page, 'html.parser')
-        price_box = soup.find('div', attrs={'class':'last-sale padded-bottom displayIB paddingR5p'})
-        price = price_box.text
-        print('Current stock price: ' + price)
-        print('\n')
-
         #Get company news
         finallist = []
         ulturl = "https://api.diffbot.com/v3/article?token=eac77c2dca2a627f47278a19e38506a6&url="
@@ -30,24 +21,13 @@ def getnews(news):
         url = 'https://newsapi.org/v2/everything?q=MSFT&from=2019-01-11&sortBy=publishedAt&apiKey=5fbb5fe295f64199ae436a70e332e335'
         json_data = requests.get(url).json()
         for i in range(0, 7):
-                source = json_data['articles'][i]['source']['name']
-                title = json_data['articles'][i]['title']
-                desc = json_data['articles'][i]['description']
                 link = json_data['articles'][i]['url']
                 fin = getarticle(ulturl + link)
 
                 if fin!= None:
                         finallist.append(fin)
                 
-
-
-        # #Get financial data
-        # url = 'https://finance.yahoo.com/quote/MSFT/analysis?p=' + news
-        # html = requests.get(url).content
-        # df_list = pd.read_html(html)
-        # for i in range(0, 6):
-        #         df = df_list[i]
-        #         print(df)
+        print(finallist)
 
         return(finallist)
         
