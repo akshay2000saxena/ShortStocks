faea468851ff9a7109c8ecfea51a2799a19ab4a6
Akshay Saxena <41586370+akshay2000saxena@users.noreply.github.com>
2019-01-13 08:26:13 +0530

    Add files via upload

---
+++ newsbot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import pandas as pd
+import string
+
+def getarticle(url):
+    json_data2 = requests.get(url).json()
+    result = json_data2['objects'][0]['text']
+    final = [word.strip(string.punctuation) for word in result.split()]
+    return final
+
+news = input('Company Stockname: ')
+print('\n')
+
+#Get company stock price
+quote_page = 'https://m.nasdaq.com/symbol/' + news
+page = urlopen(quote_page)
+soup = BeautifulSoup(page, 'html.parser')
+price_box = soup.find('div', attrs={'class':'last-sale padded-bottom displayIB paddingR5p'})
+price = price_box.text
+print('Current stock price: ' + price)
+print('\n')
+
+#Get company news
+# finallist = []
+url = 'https://newsapi.org/v2/everything?q=' + news + '&from=2019-01-11&sortBy=publishedAt&apiKey=5fbb5fe295f64199ae436a70e332e335'
+json_data = requests.get(url).json()
+for i in range(0, 20):
+        source = json_data['articles'][i]['source']['name']
+        title = json_data['articles'][i]['title']
+        desc = json_data['articles'][i]['description']
+        link = json_data['articles'][i]['url']
+        # finallist += getarticle(link)
+        
+        print('Source: ' + source)
+        print('Title: ' + str(title))
+        print('Description: ' + desc)
+        print('Link: ' + str(link))
+        print('\n')
+
+
+
+# #Get financial data
+url = 'https://finance.yahoo.com/quote/MSFT/analysis?p=' + news
+html = requests.get(url).content
+df_list = pd.read_html(html)
+for i in range(0, 6):
+        df = df_list[i]
+        print(df)
+
+# print(finallist)
+input('')
