655c70e72081a7e23e7b1fcc02d8f87b6f368a78
navyamehta <45111560+navyamehta@users.noreply.github.com>
2019-01-13 09:13:53 -0500

    Update task.py

--- task.py
+++ task.py
@@ -2,6 +2,7 @@ import newsbot
 import newsresults
 import classifier
 import trainer
+import chooser
 from flask import Flask, request, render_template
 from sklearn.neural_network import MLPClassifier
 import getall
@@ -20,26 +21,13 @@ def my_form_post():
     tag2d = []
     for lst in articles:
         tag2d.append(classifier.classifier(lst))
-    
     full = getall.getall("Navya")
-
     X = full[0]
-
     y = full[1]
-
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, 40, 10, 2), random_state=1)
-
     clf.fit(X, y)
-
     relevance_array = clf.predict(tag2d)
-
     artprint = newsbot.getarticle(text)
-    finalarr = []
-    
-    for i in range(0, len(helpval)):
-        if (helpval[i] == 1):
-            finalarr.append(artprint[i])
-
+    finalarr = chooser.chooser(relevance_array, artprint)
     print(finalarr)
-
     return "blah", 200
