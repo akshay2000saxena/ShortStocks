874c543de5141a4958c88fb36f7ba9cd1612bf1f
rkaushik29 <rohit.rk.rk1@gmail.com>
2019-01-12 21:01:02 -0500

    Add files via upload

--- trainer.py
+++ trainer.py
@@ -4,7 +4,7 @@
 
 from sklearn.neural_network import MLPClassifier
 	
-X = [[2, 1], [2, 1], [3, 0], [2, 1]]
+X = [[2, 1], [2, 1], [3, 0], [2, 0]]
 y = [1, 1, 0, 0]
 
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, 40, 10, 2), random_state=1)
@@ -13,7 +13,9 @@ clf.fit(X, y)
 
 print(clf.predict([[2, 1], [3, 1]]))
 
-            
+print("Accuracy is: ")
+print(clf.score(X,y) * 100)
+
 
 
 
