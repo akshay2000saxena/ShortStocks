35c5efd51e501e45ee5715d5a226eb0a683e5038
Akshay Saxena <41586370+akshay2000saxena@users.noreply.github.com>
2019-01-13 14:04:17 +0530

    Flask

--- newsresults.py
+++ newsresults.py
@@ -6,6 +6,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import string
 
+
+def getarticle(url):
+    json_data2 = requests.get(url).json()
+    result = json_data2['objects'][0]['text']
+    final = [word.strip(string.punctuation) for word in result.split()]
+    return final
+
 def getnews(news):
         #Get company stock price
         quote_page = 'https://m.nasdaq.com/symbol/' + news
@@ -13,12 +20,11 @@ def getnews(news):
         soup = BeautifulSoup(page, 'html.parser')
         price_box = soup.find('div', attrs={'class':'last-sale padded-bottom displayIB paddingR5p'})
         price = price_box.text
-        sub = ""
-        sub += ('Current stock price: ' + price)
-        sub += ('\n')
-        sub += '\n'
+        print('Current stock price: ' + price)
+        print('\n')
 
         #Get company news
+        sub = {}
 
         url = 'https://newsapi.org/v2/everything?q=MSFT&from=2019-01-11&sortBy=publishedAt&apiKey=5fbb5fe295f64199ae436a70e332e335'
         json_data = requests.get(url).json()
@@ -27,16 +33,15 @@ def getnews(news):
                 title = json_data['articles'][i]['title']
                 desc = json_data['articles'][i]['description']
                 link = json_data['articles'][i]['url']
+
+                article = {
+                    'source': source,
+                    'title': title,
+                    'desc': desc,
+                    'link' : link
+                }
                 
-                sub += ('Source: ' + source)
-                sub += '\n'
-                sub += ('Title: ' + str(title))
-                sub += '\n'
-                sub += ('Description: ' + desc)
-                sub += '\n'
-                sub += ('Link: ' + str(link))
-                sub += '\n'
-                sub += '\n'
+                sub[i] = article
 
 
         # #Get financial data
