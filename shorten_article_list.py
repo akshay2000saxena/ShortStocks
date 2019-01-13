1da5565ff09efe92de7df9e9455590cc1b21ffcc
rkaushik29 <rohit.rk.rk1@gmail.com>
2019-01-13 01:24:56 -0500

    Add files via upload

---
+++ shorten_article_list.py
@@ -0,0 +1,10 @@
+article_prime = []
+k = 0
+
+for i in article:
+	if len(i) <= 3:
+		pass
+	else:
+		article_prime[k] = i
+		k + 1
+
--- trainer.py
+++ trainer.py
@@ -3,19 +3,22 @@
 # Machine Learning for ShortStocks
 
 from sklearn.neural_network import MLPClassifier
+import getall
 	
-X = [[2, 1], [2, 1], [3, 0], [2, 0]]
-y = [1, 1, 0, 0]
+full = getall.getall("Navya")
+
+X = full[0]
+
+y = full[1]
 
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(100, 40, 10, 2), random_state=1)
 
 clf.fit(X, y)
 
-print(clf.predict([[2, 1], [3, 1]]))
+print(clf.predict([[2, 0], [4, 1], [6, 0]]))
 
-print("Accuracy is: ")
-print(clf.score(X,y) * 100)
 
+    
 
 
 
